c559f41f863933fb8da43c639439d6918beed86d
Sonia <149040956+desamsonia@users.noreply.github.com>
2025-03-04 21:58:00 +0530

    without_main.c

--- without_main.c
+++ without_main.c
@@ -1,3 +1,6 @@
+//we can compile and execute without main function in c, but it is not recommended.
+//compile using gcc -nostartfiles without_main.c  -> this means to compile without main, we will have to create executable file without a _start func.for that using -nostartfiles
+
 #include<stdio.h>
 #include<stdlib.h>
 
